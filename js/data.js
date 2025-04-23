bf5dd0c6761e05b673e44e8d9a14675370ec2d8a
Piyush Vishwakarma <115068496+Piyushvishwak@users.noreply.github.com>
2025-04-23 23:13:38 +0530

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,117 @@
+// Project Data
+const projectsData = [
+  {
+    id: 2,
+    title: 'Gandalf Books',
+    description: 'An online bookstore platform offering a wide range of books with features like reviews, ratings, and personalized recommendations.',
+    image: 'images/bookstore.png',
+    category: 'web',
+    technologies: ['React', 'MongoDB', 'Express.js', 'Node.js'],
+    demoLink: 'https://gandalf-books-git-main-piyushs-projects-a1a62dfa.vercel.app/',
+    codeLink: 'https://github.com/Piyushvishwak/GandalfBooks'
+  },
+  {
+    id: 3,
+    title: 'Fitness Freaks',
+    description: 'An introductory website for the Fitness Freaks app promoting healthy living with workouts, videos, and testimonials.',
+    image: 'images/fitness.png',
+    category: 'web',
+    technologies: ['HTML', 'CSS', 'JavaScript'],
+    demoLink: 'https://piyushvishwak.github.io/Fitness-Freaks/',
+    codeLink: 'https://github.com/Piyushvishwak/Fitness-Freaks'
+  },
+  {
+    id: 1,
+    title: 'Artisan\'s Marketplace',
+    description: 'A dynamic marketplace for artisans to showcase and sell handmade products, with real-time chat, product management, and secure user authentication.',
+    image: 'images/artisan.png',
+    category: 'web',
+    technologies: ['EJS', 'Node.js', 'MongoDB', 'Express.js', 'Socket.io', 'Bootstrap'],
+    demoLink: '#',
+    codeLink: 'https://github.com/Piyushvishwak/Artisan-s-Marketplace'
+  },
+  {
+    id: 4,
+    title: 'Healthcare app',
+    description: 'A healthcare mobile app built using Kotlin to help users book doctor appointments, track health records, and access medical tips for a healthier lifestyle.',
+    image: 'images/android.png',
+    category: 'mobile',
+    technologies: ['Kotlin', 'Android Studio', 'Firebase', 'XML'],
+    demoLink: '#',
+    codeLink: 'https://github.com/Piyushvishwak/HealthCare-Android-App'
+  },
+  {
+    id: 5,
+    title: 'Dodgy Dave\'s stock prediction',
+    description: 'A stock market prediction model that utilizes machine learning algorithms to forecast future stock prices based on historical data.',
+    image: 'images/stockAI.png',
+    category: 'web',
+    technologies: ['Python', 'Pandas', 'NumPy', 'Matplotlib', 'Scikit-learn'],
+    demoLink: 'https://piyushvishwak.github.io/Dodge-s-Dave-Stock/',
+    codeLink: 'https://github.com/Piyushvishwak/Stock-Prediction/tree/main/stockPrediction'
+  },
+  {
+    id: 6,
+    title: 'Book Store',
+    description: 'A Figma-based bookstore prototype showcasing interactive catalog browsing, intuitive search filters, and responsive layouts for both web and mobile platforms.',
+    image: 'images/figmabook.jpg',
+    category: 'design',
+    technologies: ['Figma', 'UI/UX Design'],
+    demoLink: 'https://www.figma.com/proto/f0M5jaXtqVcebTuyRNBZJc/Gandalf-s-Books?node-id=1-385&starting-point-node-id=1%3A385&t=M44XKrWUf5LK2YT0-1',
+    codeLink: 'https://www.figma.com/design/f0M5jaXtqVcebTuyRNBZJc/Gandalf-s-Books?t=srnEF1SIrvXpl96L-0'
+  }
+];
+
+// Experience Data
+const experienceData = [
+  {
+    id: 1,
+    date: 'Jul 2024 - Dec 2024',
+    title: 'Frontend & Backend code Reviewer and Evaluator',
+    company: 'Outlier',
+    description: '• Developed and trained backend code-generation models to derive data-driven insights. <br> • Enhanced server-side algorithms to improve scalability and response times.',
+    // tags: ['React', 'Node.js', 'AWS', 'MongoDB', 'Docker']
+  },
+    {
+      id: 2,
+      date: 'Apr 2019 - Mar 2021',
+      title: 'Intermediate',
+      company: 'R.S. Convent Sainik School & Hostel, Ledhupur, Varanasi',
+      description: 'Percentage: 86.7%',
+      // tags: ['Physics', 'Chemistry', 'Mathematics']
+    },
+    {
+      id: 3,
+      date: 'Apr 2017 - Mar 2019',
+      title: 'Matriculation',
+      company: 'R.S. Convent Sainik School & Hostel, Ledhupur, Varanasi',
+      description: 'Percentage: 93.4%',
+      // tags: ['Science', 'Mathematics', 'English']
+    },
+  
+    {
+      id: 4,
+      date: '—',
+      title: 'Soft Skills',
+      company: '—',
+      description: 
+        '• Problem-Solving <br>' +
+        '• Collaboration <br>' +
+        '• Time Management <br>' +
+        '• Adaptability <br>' +
+        '• Leadership <br>' +
+        '• Critical Thinking <br>' +
+        '• Communication <br>' +
+        '• Emotional Intelligence (EQ)'
+    },
+
+    {
+      id: 5,
+      date: '—',
+      title: 'Languages',
+      company: '—',
+      description:
+        '• English <br>' +
+        '• Hindi'
+    }
+];
